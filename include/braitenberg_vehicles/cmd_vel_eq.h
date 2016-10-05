b6319a9f9aa5e48bb7bd8e5ac0fbbaefc7a97655
Prof. Lee <leegs@case.edu>
2016-10-05 23:09:04 +0100

    Version 0.0 of the braitenberg_vehicles package.
    This a package for experimenting with the theories of Valention Braitenberg using the STDR Simulator package in ROS.

---
+++ include/braitenberg_vehicles/cmd_vel_eq.h
@@ -0,0 +1,7 @@
+
+#ifndef CMD_VEL_EQ_H
+#define CMD_VEL_EQ_H
+
+void cmd_vel_eq(float g_co2_sensor_0_ppm, float g_co2_sensor_1_ppm, float *vel, float *ang_vel);
+
+#endif
---
+++ src/cmd_vel_eq.cpp
@@ -0,0 +1,11 @@
+#include "braitenberg_vehicles/cmd_vel_eq.h"
+
+
+void cmd_vel_eq(float g_co2_sensor_0_ppm, float g_co2_sensor_1_ppm, float *vel, float *ang_vel) {
+
+  *vel = 0.0; // Robot forward velocity in m/s
+  *ang_vel = 0.0;  // Robot angular velocity in rad/s
+
+  return;
+}
+
