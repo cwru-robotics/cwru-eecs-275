661c39ee68bdaf863cbd55d2a96a62bbcf1b739a
mjg152 <mjg152@case.edu>
2016-11-02 19:18:36 +0000

    Update turtleStart.sh
    Updates start up script to reference combined launch file and ros_ws setup.bash instead of the previously included jetson setup.bash.

--- cwru-turtlebot/turtleStart.sh
+++ cwru-turtlebot/turtleStart.sh
@@ -2,7 +2,8 @@
 ip=$(ip -f inet -o addr show wlan1|cut -d\  -f 7 | cut -d/ -f 1)
 echo $ip
 echo "We tried to start minimal launch"
-source /home/ubuntu/jetsonbot/devel/setup.bash
+source /home/ubuntu/ros_ws/devel/setup.bash
+export ROS_IP=$ip
 export ROS_MASTER_URI=http://$ip:11311
-roslaunch turtlebot_bringup minimal.launch
+roslaunch deeplearning_bringup deeplearning.launch
 
