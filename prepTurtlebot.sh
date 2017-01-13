f733948009377df08da59df3de135110a7e86704
mjg152 <mjg152@case.edu>
2017-01-13 16:48:53 -0500

    Update prepTurtlebot.sh
    Updates to reflect changing prepTurtlebot.sh to root directory.

--- prepTurtlebot.sh
+++ prepTurtlebot.sh
@@ -25,20 +25,20 @@ git clone https://github.com/yujinrobot/kobuki_msgs
 cd ~/ros_ws
 catkin_make
 
-mv $initialDirectory/cwru-eecs-275/minimal_turtlebot ~/ros_ws/src
-mv $initialDirectory/cwru-eecs-275/cwru_deeplearning_robot/deeplearning_bringup ~/ros_ws/src
+mv $initialDirectory/minimal_turtlebot ~/ros_ws/src
+mv $initialDirectory/cwru_deeplearning_robot/deeplearning_bringup ~/ros_ws/src
 
-mv $initialDirectory/cwru-eecs-275/cwru-turtlebot/turtleStart.sh ~/ros_ws/src/deeplearning_bringup/launch
+mv $initialDirectory/cwru-turtlebot/turtleStart.sh ~/ros_ws/src/deeplearning_bringup/launch
 
 cd ~/ros_ws
 catkin_make
 
 #~/ros_ws/src/ros_astra_camera/scripts/create_udev_rules
 
-sudo chmod +755 $initialDirectory/cwru-eecs-275/cwru-turtlebot/rc.local
-sudo mv $initialDirectory/cwru-eecs-275/cwru-turtlebot/rc.local /etc
-sudo chmod +755 /home/ubuntu/ros_ws/src/deeplearning_bringup/launch/turtleStart.sh
-sudo rm -r $initialDirectory/cwru-eecs-275/
+sudo chmod +755 $initialDirectory/cwru-turtlebot/rc.local
+sudo mv $initialDirectory/cwru-turtlebot/rc.local /etc
+sudo chmod +755 ~/ros_ws/src/deeplearning_bringup/launch/turtleStart.sh
+sudo rm -r $initialDirectory/
 #sudo apt-get install ros-indigo-astra-launch
 # Need Astra code if we're not using Asus camera 
 
