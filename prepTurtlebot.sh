b1e0e6bacdf8f0a4d512fded540d47c3532d6b1b
mjg152 <mjg152@case.edu>
2017-01-13 18:20:44 -0500

    Update prepTurtlebot.sh
    Updates to check if we're using an orbbec camera

--- prepTurtlebot.sh
+++ prepTurtlebot.sh
@@ -2,6 +2,15 @@
 ##################################################################
 ### Created by Mike Gallagher, Case Western Reserve University ###
 ##################################################################
+if $1 == "orbbec"; 
+then
+ehco "We see we have an orbbec camera"
+orbbec=true; 
+else 
+echo "We're assuming we have an asus camera"
+orbbec=false;
+fi
+
 initialDirectory=$PWD
 
 source /opt/ros/indigo/setup.bash
@@ -14,8 +23,12 @@ cd ~/ros_ws/src
 catkin_init_workspace
 
 git clone https://github.com/yujinrobot/kobuki_msgs
-#git clone https://github.com/orbbec/ros_astra_camera.git
+if orbbec; 
+then 
+echo "cloning orbbec astra code"
+git clone https://github.com/orbbec/ros_astra_camera.git
 # Need Orbbec code if we're not using Asus camera 
+fi
 
 cd ~/ros_ws
 catkin_make
@@ -34,6 +47,9 @@ sudo chmod +755 $initialDirectory/cwru-turtlebot/rc.local
 sudo mv $initialDirectory/cwru-turtlebot/rc.local /etc
 sudo chmod +755 ~/ros_ws/src/deeplearning_bringup/launch/turtleStart.sh
 sudo rm -r $initialDirectory/
-#sudo apt-get install ros-indigo-astra-launch
+if orbbec
+then
+echo "installing astra launch files"
+sudo apt-get install ros-indigo-astra-launch
 # Need Astra code if we're not using Asus camera 
-
+fi 
