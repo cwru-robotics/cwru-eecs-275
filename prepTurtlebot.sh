5f291f0539ca59bd2ab14cd67d4b69053405c30f
mjg152 <mjg152@case.edu>
2017-01-27 13:09:53 -0500

    Update prepTurtlebot.sh
    Updates the script to be expected to be run from admin.

--- prepTurtlebot.sh
+++ prepTurtlebot.sh
@@ -17,10 +17,10 @@ echo $initialDirectory
 source /opt/ros/indigo/setup.bash
 
 echo "First try to remove ros_ws"
-sudo rm -r ~/ros_ws/
-mkdir -p ~/ros_ws/src
+sudo rm -r /home/ubuntu/ros_ws/
+mkdir -p /home/ubuntu/ros_ws/src
 
-cd ~/ros_ws/src
+cd /home/ubuntu/ros_ws/src
 catkin_init_workspace
 
 git clone https://github.com/yujinrobot/kobuki_msgs
@@ -30,36 +30,36 @@ echo "cloning orbbec astra code"
 git clone https://github.com/orbbec/ros_astra_camera.git
 git clone https://github.com/orbbec/ros_astra_launch.git
 
-source ~/ros_ws/devel/setup.bash 
-cd ~/ros_ws
+source /home/ubuntu/ros_ws/devel/setup.bash 
+cd /home/ubuntu/ros_ws
 catkin_make --pkg astra_camera -DFILTER=OFF
 # Let's force the filter to be off, should be by default but let's build here just to be sure
-cd ~/ros_ws/src
+cd /home/ubuntu/ros_ws/src
 fi
 
-mv $initialDirectory/minimal_turtlebot ~/ros_ws/src
-mv $initialDirectory/cwru_deeplearning_robot/deeplearning_bringup ~/ros_ws/src
+mv $initialDirectory/minimal_turtlebot /home/ubuntu/ros_ws/src
+mv $initialDirectory/cwru_deeplearning_robot/deeplearning_bringup /home/ubuntu/ros_ws/src
 
-mv "$initialDirectory/cwru-turtlebot/turtleStart.sh" ~/ros_ws/src/deeplearning_bringup/launch
+mv "$initialDirectory/cwru-turtlebot/turtleStart.sh" /home/ubuntu/ros_ws/src/deeplearning_bringup/launch
 
 sudo chmod +755 "$initialDirectory/cwru-turtlebot/rc.local"
 sudo mv "$initialDirectory/cwru-turtlebot/rc.local" /etc
-sudo chmod +755 ~/ros_ws/src/deeplearning_bringup/launch/turtleStart.sh
+sudo chmod +755 /home/ubuntu/ros_ws/src/deeplearning_bringup/launch/turtleStart.sh
 sudo rm -r $initialDirectory/
 
-cd ~/ros_ws
+cd /home/ubuntu/ros_ws
 catkin_make # call this first to complete ws initialization
 
-source ~/ros_ws/devel/setup.bash 
-cd ~/ros_ws
+source /home/ubuntu/ros_ws/devel/setup.bash 
+cd /home/ubuntu/ros_ws
 catkin_make
 
 
 if [ "$1" == "orbbec" ]; 
 then
 echo "installing astra launch files"
-~/ros_ws/src/ros_astra_camera/scripts/create_udev_rules
-sudo mv ~/ros_ws/src/deeplearning_bringup/launch/deeplearning_orbbec.launch ~/ros_ws/src/deeplearning_bringup/launch/deeplearning.launch
+/home/ubuntu/ros_ws/src/ros_astra_camera/scripts/create_udev_rules
+sudo mv /home/ubuntu/ros_ws/src/deeplearning_bringup/launch/deeplearning_orbbec.launch /home/ubuntu/ros_ws/src/deeplearning_bringup/launch/deeplearning.launch
 
 # Need Astra code if we're not using Asus camera 
 fi 
