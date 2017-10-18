6356630a11dc3ef910d9ae4ed9051df3f8afda09
mjg152 <mjg152@case.edu>
2017-10-18 11:00:34 -0400

    Updates to launch with MS Kinect

---
+++ cwru_deeplearning_robot/deeplearning_bringup/launch/turtleStart.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+ip=$(ip -f inet -o addr show wlan1|cut -d\  -f 7 | cut -d/ -f 1)
+echo $ip
+echo "We tried to start minimal launch"
+source /home/ubuntu/ros_ws/devel/setup.bash
+export ROS_IP=$ip
+export ROS_MASTER_URI=http://$ip:11311
+roslaunch deeplearning_bringup deeplearning.launch
